adf1df91d748b4522a55427ebcad758b3a3fb79c
Wiktor <Wiktor@DESKTOP-8DRIGKV>
2022-12-16 20:19:21 +0100

    Azure functions project part 1

---
+++ AzureFunctionTangyWeb/Models/SalesRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace AzureFunctionTangyWeb.Models
+{
+    public class SalesRequest
+    {
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Status { get; set; }
+    }
+}
